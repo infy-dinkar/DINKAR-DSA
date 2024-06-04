e38d331faad2e7698e0f5f6c91ea959224b83b36
infy-dinkar <139960460+infy-dinkar@users.noreply.github.com>
2024-06-04 12:01:56 +0530

    Add files via upload

---
+++ TargetSearchIn2DVector.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == target) {
+                    return true; 
+                }
+            }
+        }
+        return false; 
+    }
+};
+
