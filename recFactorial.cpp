749514588e0a416cce4f9948003ddcd61964dd8f
infy-dinkar <139960460+infy-dinkar@users.noreply.github.com>
2024-07-01 20:44:10 +0530

    Add files via upload

---
+++ recFactorial.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n) {
+    if(n == 0 || n == 1) return 1;
+    return n * fact(n - 1);
+}
+
+int main() {
+    int result = fact(5);
+    cout << result << endl;
+    return 0;
+}
