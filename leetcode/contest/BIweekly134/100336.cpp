5d666c5becff211f97f7bf25a1e331dd9d5f7540
Dinkar Thakur <322dinkar@gmail.com>
2024-07-07 19:51:55 +0530

    change

--- leetcode/contest/BIweekly134/100336.cpp
+++ leetcode/contest/BIweekly134/100336.cpp
@@ -1,14 +1,15 @@
+#include <vector>
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors) {
         int n = colors.size();
     int count = 0;
     for (int i = 0; i < n; ++i) {
-        int pre = colors[i];
+        int prev = colors[i];
             int cur = colors[(i + 1) % n];
             int next = colors[(i + 2) % n];
         
-        if (pre != cur && cur!= next ) {
+        if (prev != cur && cur!= next ) {
             ++count;
         }
     }
